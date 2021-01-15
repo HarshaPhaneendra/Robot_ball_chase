c1931fd4a771107b1796ef54360fd29614b9e055
HarshaPhaneendra <harshap812@gmail.com>
2021-01-15 19:31:55 +0100

    issue with image partition

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -1,9 +1,11 @@
 #include "ros/ros.h"
 #include "ball_chaser/DriveToTarget.h"
-#include "sensor_msgs/Image.h"
+#include <sensor_msgs/Image.h>
+#include "std_msgs/String.h"
 
 ros::ServiceClient client;
 
+// function to drive robot via service
 void drive_robot(float lin_x, float ang_z)
 {
     ROS_INFO("Driving robot to target");
@@ -18,45 +20,98 @@ void drive_robot(float lin_x, float ang_z)
 
 }
 
+// calback funtion 
 void process_image_callback(const sensor_msgs::Image img)
 {
+    ROS_INFO("process_image_callback() is started ");
+
     int white_pixel = 255;
     bool white_pixel_found = false;
     enum split_image{left, middle, right}; 
     split_image side;
-    int matrix_image[img.height][img.width] = { };
+    //int matrix_image[img.height][img.width] = { };
+
+    for(int i=0; i < img.height*(img.step/3); i++)
+    {
+        if(img.data[i] == white_pixel)
+        {
+            side = left;
+            ROS_INFO("White pixel found at left side of Image!!");
+            break;
+        }
+    }
+
+    for(int i=img.height*(img.step/3); i < img.height*(2*img.step/3); i++)
+    {
+        if(img.data[i] == white_pixel)
+        {
+            side = middle;
+            ROS_INFO("White pixel found at middle of Image!!");
+            break;
+        }
+    }
+
+    for(int i=img.height*(2*img.step/3); i < img.height*img.step; i++)
+    {
+        if(img.data[i] == white_pixel)
+        {
+            side = right;
+            ROS_INFO("White pixel found at right side of Image!!");
+            break;
+        }
+    }
+
     
-    // converting given image into matrix format
+    /* // converting given image into matrix format
     for(int i=0; i < img.height; i++) //traversing through image rows
     {
         for(int j=0; j < img.width; j++)// traversing through image columns
         {
-
-            matrix_image[i][j] = img.data[(i*img.height) + j];
+            if(img.data[(i*img.width) + j] == 255)
+            {
+                ROS_INFO("White pixel found!!");
+                break;
+            }
+            // matrix_image[i][j] = img.data[(i*img.width) + j]; 
         }
-    }
+    } */
+
+    /*ROS_INFO("Image is converted to matrix format");
             
     // to find white pixel in image
     for(int i=0; i < img.height; i++) //traversing through image rows
     {
         for(int j=0; j < img.width; j++)// traversing through image columns
         {
-            if(matrix_image[i][j] == 255)
+            if(matrix_image[i][j] == white_pixel)
             {
                 white_pixel_found = true;
+                ROS_INFO("White pixel found!!");
+
                 if(j < (img.width/3))
-                   side = left;
+                {
+                    side = left;
+                    ROS_INFO("White pixel found on left side of image");
+
+                }
                 else if((img.width/3) < j < 2*(img.width/3))
+                {
                     side = middle;
-                else    
+                    ROS_INFO("White pixel found at middel of image");
+                }    
+                else
+                {
                     side = right;
-                
+                    ROS_INFO("White pixel found on right side of image");
+                }    
+                               
                 break;
             }
         }
-    }
+    } */
 
-    if(white_pixel_found == true)
+    // driving robot 
+    /* if(white_pixel_found == true)
     {
         switch (side)
         {
@@ -78,8 +133,11 @@ void process_image_callback(const sensor_msgs::Image img)
         }
     }
     else
+    {
         drive_robot(0,0);
-        ROS_INFO("No white pixel found!!, STOP the robot");
+        ROS_INFO("No white pixel found!!, STOP the robot"); 
+    } */
+        
 
 
 }
